1b3f8982ed84368d4aba7b61dec914a306e38412
sushberiwal <sushantberiwal@gmail.com>
2020-10-22 20:21:21 +0530

    download complete

--- Lec8_whiteboard/activity/image.js
+++ Lec8_whiteboard/activity/image.js
@@ -1,8 +1,7 @@
 let imgUpload = document.querySelector("#imgUpload");
-
+let download = document.querySelector("#download-img");
 
 imgUpload.addEventListener("change" , function(){
-  
       let src = URL.createObjectURL(imgUpload.files[0]);
       console.log(src);
       let img = document.createElement("img"); // <img src=""     />  
@@ -10,4 +9,12 @@ imgUpload.addEventListener("change" , function(){
       img.setAttribute("class" , "img-upload");
       let content = createSticky();
       content.appendChild(img);
+})
+
+
+download.addEventListener("click" , function(){
+  let aTag = document.createElement('a');  // <a href="" > </a>
+  aTag.download = 'filename.png';         // <a href="" download = "filename.png" > </a>
+  aTag.href = canvas.toDataURL("image/png"); // <a href="image ki link" download = "filename.png" > </a>
+  aTag.click();
 })

23a1e559730ced35ebb1e5187f7278dfe2bf9f13
sushberiwal <sushantberiwal@gmail.com>
2020-10-23 21:55:39 +0530

    user crud complete

--- Lec9_Instagram/Backend/app.js
+++ Lec9_Instagram/Backend/app.js
@@ -18,20 +18,26 @@ const app = express();
 // if you want to see data in req.body
 app.use(express.json());
 
+
+
+
+// CRUD => create read update delete => user
+
+
+
 // create a user => details aayengi req.body
 app.post("/user" , function(req,res){
     let uid = uuidv4();
     let newUser = req.body;
     newUser.uid = uid;
     userDB.push(newUser);
-    fs.writeFileSync("./db/userDB.json" , JSON.stringify(userDB));
+    fs.writeFileSync("./db/users.json" , JSON.stringify(userDB));
     // send a response 
     res.json({
         message : "Added a user succesfully",
         data : newUser
     })
 })
-
 // get all userDB
 app.get("/user" , function(req , res){
     if(userDB.length){
@@ -46,7 +52,6 @@ app.get("/user" , function(req , res){
         })
     }
 });
-
 
 // get by id
 app.get("/user/:uid" , function(req,res){
@@ -68,20 +73,47 @@ app.get("/user/:uid" , function(req,res){
      }
 })
 
-
-
-
 // update by id
-app.patch("/user" , function(req,res){
+app.patch("/user/:uid" , function(req,res){
 
+    let uid = req.params.uid;
+    let updateObject = req.body;
 
+    let userToBeUpdated = userDB.filter(function(user){
+        return user.uid == uid;
+    })
+
+    for(key in updateObject){
+        userToBeUpdated[0][key] = updateObject[key];
+    }
+    fs.writeFileSync("./db/users.json" , JSON.stringify(userDB));
+    res.json({
+        message:"User updated succesfully",
+        data : userToBeUpdated[0]
+    })
 })
 
-
-
 // delete by id
-app.delete("/user" , function(req,res){
-
+app.delete("/user/:uid" , function(req,res){ 
+    let uid = req.params.uid;
+    let userToBeDeleted = userDB.filter( function(user){
+        return user.uid == uid;
+    });
+    let filteredUsers = userDB.filter( function(user){
+        return user.uid != uid;
+    });
+    if(filteredUsers.length == userDB.length){
+        res.json({
+            message:"user not found !!"
+        })
+    }
+    else{
+        fs.writeFileSync("./db/users.json" , JSON.stringify(filteredUsers));
+        res.json({
+            message:"User deleted Succesfully",
+            data : userToBeDeleted[0]
+        })
+    }
 
 })
 
